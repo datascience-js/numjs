572c6737cac67ee8ff83b5bebe2aeefd8fbf9f22
unknown <shirihaim@gmail.com>
2015-03-29 12:50:31 +0300

    pull commit

--- samples/gaussian_elimination.js
+++ samples/gaussian_elimination.js
@@ -6,9 +6,9 @@ class GaussianElimination{
         var ratio, b = new Vector();
         for (var i = 0; i < matrixToCalc.shape()[0]-1; i++) {
             for (var j = i; j < matrixToCalc.shape()[1]; j++) {
-                ratio = matrixToCalc[j][i] / matrixToCalc[i][i];
+                ratio = matrixToCalc.at(j, i) / matrixToCalc.at(i, i);
                 for (var k = i; k < matrixToCalc.shape()[0]; k++) {
-                    matrixToCalc[j][k] -= (ratio * matrixToCalc[i][k]);
+                    matrixToCalc[j][k] -= (ratio * matrixToCalc.at(i, k));
                     b[j] -= (ratio * b[i]);
                 }
             }
